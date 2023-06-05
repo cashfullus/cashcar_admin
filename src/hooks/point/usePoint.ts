131d065ae1b53ec0e0b283f2dd84f2a662c30cf2
ordidxzero <dev@dlwlrma.net>
2023-06-05 13:35:30 +0900

    fix bug

--- src/hooks/point/usePoint.ts
+++ src/hooks/point/usePoint.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { PAGE_SIZE_10 } from 'components/shared/Pagination';
 import { RootState } from 'lib/modules';
 import { deselectPointListAction, getPointListAsync, selectPointListAction, togglePointAction } from 'lib/modules/point-overview';
@@ -7,13 +7,19 @@ import { useDispatch, useSelector } from 'react-redux';
 import { filterDataFormatter } from 'lib/tools';
 
 const usePoint = () => {
+  const [filterS, setFilterS] = useState({});
   const dispatch = useDispatch();
   const items = useSelector((state: RootState) => state.point.items);
   const filter = useSelector((state: RootState) => state.filter.pointOverview);
+  useEffect(() => {
+    setFilterS(filterDataFormatter(filter))
+  }, [filter])
   const selected = useSelector((state: RootState) => state.point.selected);
   const itemCount = useSelector((state: RootState) => state.point.item_count);
   const loading = useSelector((state: RootState) => state.loading.getPointList);
-  const getPointList = useCallback((payload: GetPointListPayload) => dispatch(getPointListAsync.request({...payload, ...filterDataFormatter(filter)})), [dispatch, filter]);
+  const getPointList = useCallback((payload: GetPointListPayload) => {
+    dispatch(getPointListAsync.request({...payload, ...filterS}));
+  }, [dispatch, filter]);
   const togglePoint = useCallback((userId: number | string) => dispatch(togglePointAction(+userId)), [dispatch]);
   const selectPointList = useCallback(() => dispatch(selectPointListAction()), [dispatch]);
   const deselectPointList = useCallback(() => dispatch(deselectPointListAction()), [dispatch]);
